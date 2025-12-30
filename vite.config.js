66bdbca82bf3546ed9d7e3b6635e1efab44a9573
Padmanabansals <padmanabansals.net@gmail.com>
2025-12-30 22:34:07 +0530

    port changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: Number(process.env.PORT) || 5173,
+    strictPort: true,
+  },
+  preview: {
+    host: true,
+    port: Number(process.env.PORT) || 4173,
+    strictPort: true,
+  },
 })
